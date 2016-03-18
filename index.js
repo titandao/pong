41c068c9ad245ea10b666bd115d563516d166ebb
Misrab Faizullah-Khan <faizullah.misrab@gmail.com>
2016-03-18 14:45:45 +0800

    commit

--- index.js
+++ index.js
@@ -6,14 +6,34 @@ $.fn.center = function () {
 }
 
 
+$.fn.fully_center = function() {
+   // this.css("position","absolute");
+   this.css("top", ( $(window).height() - this.height() ) / 2  + "px");
+   this.css("left", ( $(window).width() - this.width() ) / 2 + "px");
+   return this;
+}
+
+
 $(function() {
-	var w_h = $(window).height();
+	var window_h = $(window).height();
 	var racket1 = $('#racket1');
+	var racket_h = racket1.height();
+	var playing_field = $( "#playing_field" );
+	var playing_field_h = playing_field.height();
+	var ball = $('#ball');
+
+	// center the rackets initially
 	$('.racket').center();
+	ball.fully_center();
 
 
-	$( "#playing_field" ).mousemove(function( event ) {
-		racket1.css("top", Math.min(w_h * 0.5 , Math.max(w_h * 0.1, event.pageY)) );
+	// caps
+	var cap_top = 0.1 * playing_field_h + playing_field.position().top;
+	var cap_bottom = 0.9 * playing_field_h - racket_h + playing_field.position().top;
+
+
+	playing_field.mousemove(function( event ) {
+		racket1.css("top", Math.min(cap_bottom , Math.max(cap_top, event.pageY)) );
 		// console.log(event.pageY);
 	});
 
